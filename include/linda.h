d35b70c3e12203c1189c22aaa3ebe3d5b9aeaa65
michto <michto@poczta.onet.pl>
2016-06-04 20:25:23 +0200

    naglowki poprawka na variadic args

--- include/linda.h
+++ include/linda.h
@@ -4,8 +4,8 @@
 #define TUPLE_SIZE	128
 
 int linda_output(char * infoString, ...);
-char* linda_input(int timeout, char* krotka);
-char* linda_read(int timeout, char* krotka);
+char* linda_input(int timeout, char* match_string, ...);
+char* linda_read(int timeout, char* match_string, ...);
 int linda_init();
 int linda_end();
 
