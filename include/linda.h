3be454c95007b2c1684d88f84d0fb2609edbd753
michto <michto@poczta.onet.pl>
2016-06-07 23:55:22 +0200

    naglowki

--- include/linda.h
+++ include/linda.h
@@ -1,11 +1,17 @@
 
 #ifndef TEST_LINDA_H
 #define TEST_LINDA_H
+
+#include <stdbool.h>
+
 #define TUPLE_SIZE	128
 
-int linda_output(char * infoString, ...);
-char* linda_input(int timeout, char* match_string, ...);
-char* linda_read(int timeout, char* match_string, ...);
+//zwaraca czy udało się wstawić krotkę
+bool linda_output(char * infoString, ...);
+//zwraca czy znaleziono krotkę zgodną z podanym wzorcem lub timeout
+bool linda_input(int timeout, char* match_string, ...);
+//zwraca czy znaleziono krotkę zgodną z podanym wzorcem lub timeout
+bool linda_read(int timeout, char* match_string, ...);
 int linda_init();
 int linda_end();
 
